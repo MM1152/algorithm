093f13f49331113c0f1faec5063cd730ccc96072
whiteday0314 <kjy031412@naver.com>
2023-09-21 19:04:49 +0900

    인트로 , 선택 씬 제작
    인트로 끝 , 선택 씬 버튼 이벤트 완성

--- Assets/Script/LoadScene.cs
+++ Assets/Script/LoadScene.cs
@@ -1,20 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public void SceneChange()
     {
         SceneManager.LoadScene("MainScene");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
