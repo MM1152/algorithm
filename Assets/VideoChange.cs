d452d4a9b07e429131464545a15d57c3cc91cb62
MM1152 <albert6306@naver.com>
2024-02-13 15:23:50 +0900

    shot

---
+++ Assets/VideoChange.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+public class VideoChange : MonoBehaviour
+{
+    [SerializeField]
+    private VideoData videoData;
+    public VideoData VideoData { set { videoData = value; } }
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < videoData.Videos.Length; i++)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
