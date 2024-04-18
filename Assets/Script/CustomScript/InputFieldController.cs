01e65e8c62106694c2fa23e80009ffd2f3e55801
whiteday0314 <kjy031412@naver.com>
2024-04-18 18:20:41 +0900

    커스텀씬 인풋 아웃풋 박스 추가
    커스텀씬 인풋 아웃풋 박스 추가

--- Assets/Script/CustomScript/InputFieldController.cs
+++ Assets/Script/CustomScript/InputFieldController.cs
@@ -4,51 +4,81 @@ using System.Collections.Generic;
 
 public class InputFieldController : MonoBehaviour
 {
-    public Button saveButton; // 저장 버튼
-    public GameObject inputFieldObjectPrefab; // 인풋 필드를 포함한 게임 오브젝트 프리팹
+    private GameObject InputPosition;
+    private GameObject OutputPosition;
 
-    private List<GameObject> inputFieldObjects = new List<GameObject>(); // 생성된 인풋 필드를 포함한 게임 오브젝트를 저장할 리스트
-    private List<int[]> numbersList = new List<int[]>(); // 각각의 인풋 필드에서 입력한 숫자를 저장할 리스트
+    public Button saveButton; // 저장 버튼
+
+    public GameObject inputFieldObjectPrefab; // 인풋 필드를 포함한 게임 오브젝트 프리팹
+    public GameObject outputFieldObjectPrefab;
+
+    private List<InputField> inputFields = new List<InputField>(); // 생성된 인풋 필드를 저장할 리스트
+    private List<InputField> outputFields = new List<InputField>(); // 정답숫자를 입력할 인풋 필드를 저장할 리스트
 
     private void Start()
     {
-        saveButton.onClick.AddListener(SaveToJson); // 저장 버튼에 SaveToJson 메서드를 연결
+        InputPosition = GameObject.Find("InputPosition");
+        OutputPosition = GameObject.Find("OutputPosition");
+
+        saveButton.onClick.AddListener(SaveToArray); // 저장 버튼에 SaveToArray 메서드를 연결
     }
 
+
     // 저장 버튼을 클릭했을 때 호출되는 메서드
-    private void SaveToJson()
+    public void SaveToArray()
     {
-        // 각각의 인풋 필드에서 입력된 숫자를 배열로 저장하여 리스트에 추가
-        foreach (GameObject inputFieldObject in inputFieldObjects)
+        for (int i = 0; i < InputPosition.transform.childCount; i++)
         {
-            InputField inputField = inputFieldObject.GetComponent<InputField>();
-            if (inputField != null)
-            {
-                string[] numberStrings = inputField.text.Split(',');
-                List<int> numbers = new List<int>();
-                foreach (string str in numberStrings)
-                {
-                    int number;
-                    if (int.TryParse(str, out number))
-                    {
-                        numbers.Add(number);
-                    }
-                }
-                numbersList.Add(numbers.ToArray());
-            }
+            inputFields.Add(InputPosition.transform.GetChild(i).GetChild(0).GetChild(0).GetComponent<InputField>());
         }
 
-        // 리스트를 JSON 형식으로 직렬화
-        string jsonData = JsonUtility.ToJson(numbersList);
+        for (int i = 0; i < OutputPosition.transform.childCount; i++)
+        {
+            outputFields.Add(OutputPosition.transform.GetChild(i).GetChild(0).GetChild(0).GetComponent<InputField>());
+        }
+        // 각각의 인풋 필드에서 입력된 숫자를 배열로 저장하여 리스트에 추가
 
-        // 필요한 경우 jsonData를 저장하거나 전송할 수 있습니다.
-        Debug.Log("JSON 데이터: " + jsonData);
+        List<int> inputnumbersList = new List<int>();
+
+        List<int> outputnumbersList = new List<int>();
+
+        foreach (InputField inputField in inputFields)
+        {
+            inputnumbersList.Add(int.Parse(inputField.text));
+        
+        }
+
+        foreach (InputField outputField in outputFields)
+        {
+            outputnumbersList.Add(int.Parse(outputField.text));
+        }
+
+        // 배열 형식으로 저장된 숫자 리스트를 출력
+        foreach (var number1 in inputnumbersList)
+        {
+            
+            Debug.Log("저장한 입력 배열: " + number1);
+        }
+
+        foreach (var number2 in outputnumbersList)
+        {
+            Debug.Log("저장한 정답 배열 : " + number2);
+        }
     }
 
     // 사용자가 입력한 숫자를 추적하기 위해 각각의 인풋 필드를 추가하는 메서드
     public void AddInputField()
     {
         GameObject newInputFieldObject = Instantiate(inputFieldObjectPrefab, transform);
-        inputFieldObjects.Add(newInputFieldObject);
+
+        GameObject newOutputFieldObject = Instantiate(outputFieldObjectPrefab, transform);
+
+        InputField newInputField = newInputFieldObject.GetComponentInChildren<InputField>();
+
+        InputField newOutputField = newOutputFieldObject.GetComponentInChildren<InputField>();
+
+        inputFields.Add(newInputField);
+
+        inputFields.Add(newOutputField);
     }
 }
